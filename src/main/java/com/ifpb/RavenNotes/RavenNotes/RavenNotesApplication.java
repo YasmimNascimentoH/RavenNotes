79d13df8656e5ae31234532818ac2ba9446810b7
Yasmim do Nascimento Herculano <32883731+YasmimNascimentoH@users.noreply.github.com>
2024-08-22 09:53:01 -0300

    Add files via upload

---
+++ src/main/java/com/ifpb/RavenNotes/RavenNotes/RavenNotesApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.RavenNotes.RavenNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavenNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavenNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpb/RavenNotes/RavenNotes/RavenNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.RavenNotes.RavenNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavenNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
